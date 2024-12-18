c4713230775bd267135f6aef34f94fe0a760c031
Sean Cravener <scrav19@wgu.edu>
2024-12-17 18:06:03 -0800

    Added if statement check needed to meet the note in Requirement I.

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -23,24 +23,28 @@ public class BootStrapData implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Division div_one = divisionRepository.findAll().get(1);
-        Division div_two = divisionRepository.findAll().get(2);
+        // If statement needed to make sure sample customers to do get overwritten.
+        // Database is created with one initial customer, so 1 is needed in If Statement instead of 0 (empty).
+        if(customerRepository.count() == 1) {
+            Division div_one = divisionRepository.findAll().get(1);
+            Division div_two = divisionRepository.findAll().get(2);
 
-        Customer chewy = new Customer('Chewy', 'Cravener', '123 Treats Ave.', '112233', '724-223-8273', div_one);
-        Customer laney = new Customer('Laney', 'Kohler', '777 Barks Blvd', '384732', '237-384-3829', div_two);
-        Customer kirsten = new Customer('Kirsten', 'Bones', '483 Fine Street', '849202', '837-398-3889', div_one);
-        Customer bella = new Customer('Bella', 'Toy', '999 Loud Rd.', '394820', '090-884-9090', div_two);
-        Customer sonic = new Customer('Sonic', 'Cricket', '323 Lizard Lane', '876590', '444-888-6969', div_one);
+            Customer chewy = new Customer('Chewy', 'Cravener', '123 Treats Ave.', '112233', '724-223-8273', div_one);
+            Customer laney = new Customer('Laney', 'Kohler', '777 Barks Blvd', '384732', '237-384-3829', div_two);
+            Customer kirsten = new Customer('Kirsten', 'Bones', '483 Fine Street', '849202', '837-398-3889', div_one);
+            Customer bella = new Customer('Bella', 'Toy', '999 Loud Rd.', '394820', '090-884-9090', div_two);
+            Customer sonic = new Customer('Sonic', 'Cricket', '323 Lizard Lane', '876590', '444-888-6969', div_one);
 
-        customerRepository.save(chewy);
-        customerRepository.save(laney);
-        customerRepository.save(kirsten);
-        customerRepository.save(bella);
-        customerRepository.save(sonic);
+            customerRepository.save(chewy);
+            customerRepository.save(laney);
+            customerRepository.save(kirsten);
+            customerRepository.save(bella);
+            customerRepository.save(sonic);
+        }
 
-        System.out.println('BootStrapData.java')
-        System.out.println('Number of Customers' + customerRepository.count());
-        System.out.println(customerRepository.findAll());
+            System.out.println('BootStrapData.java')
+            System.out.println('Number of Customers' + customerRepository.count());
+            System.out.println(customerRepository.findAll());
 
     }
 }
