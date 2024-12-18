50c7ceebba5ba133850ef5365124a019f3fee559
Sean Cravener <scrav19@wgu.edu>
2024-12-17 17:56:34 -0800

    Finished code to add five sample customers for Requirement I.

---
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -0,0 +1,46 @@
+package com.example.demo.bootstrap;
+
+import com.example.demo.dao.CustomerRepository;
+import com.example.demo.dao.DivisionRepository;
+import com.example.demo.entity.Customer;
+import com.example.demo.entity.Division;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final CustomerRepository customerRepository;
+
+    private final DivisionRepository divisionRepository;
+
+    public BootStrapData(CustomerRepository customerRepository, DivisionRepository divisionRepository) {
+        this.customerRepository = customerRepository;
+        this.divisionRepository = divisionRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Division div_one = divisionRepository.findAll().get(1);
+        Division div_two = divisionRepository.findAll().get(2);
+
+        Customer chewy = new Customer('Chewy', 'Cravener', '123 Treats Ave.', '112233', '724-223-8273', div_one);
+        Customer laney = new Customer('Laney', 'Kohler', '777 Barks Blvd', '384732', '237-384-3829', div_two);
+        Customer kirsten = new Customer('Kirsten', 'Bones', '483 Fine Street', '849202', '837-398-3889', div_one);
+        Customer bella = new Customer('Bella', 'Toy', '999 Loud Rd.', '394820', '090-884-9090', div_two);
+        Customer sonic = new Customer('Sonic', 'Cricket', '323 Lizard Lane', '876590', '444-888-6969', div_one);
+
+        customerRepository.save(chewy);
+        customerRepository.save(laney);
+        customerRepository.save(kirsten);
+        customerRepository.save(bella);
+        customerRepository.save(sonic);
+
+        System.out.println('BootStrapData.java')
+        System.out.println('Number of Customers' + customerRepository.count());
+        System.out.println(customerRepository.findAll());
+
+    }
+}
